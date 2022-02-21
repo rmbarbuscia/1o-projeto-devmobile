83b8ba48659afde40fa1bdb0e18e2ed5a8e8c49d
rmbarbuscia <rmbarbuscia@gmail.com>
2022-02-21 11:41:25 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Olá Mundo!")
+    println("Olá Mundo!")
+    println("Olá Mundo!")
+    print("olá Mundo!\n")
+    println("joão Como vai Luiza")
+    println("Luiza estou exelente")
+    println("joão assisti um novo episodio da série Dois Homens e meio ")
+    println("Luiza blz amanhã nos falamos mais")
+    println("João ok tchau")
+
+}
